fe698cc5bd1e422a6c264b19b53c207b8e0c2c09
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-11-22 22:42:34 -0500

    It's something

---
+++ MyString.java
@@ -0,0 +1,53 @@
+public class MyString implements CharSequence{
+  //Constructer
+
+
+
+public MyString (CharSequence e){
+
+}
+
+
+
+
+
+
+
+
+
+  //
+  // public char charAt (int index) {
+  //   ;
+  // }
+  // public int length () {
+  //   ;
+  // }
+  // public CharSequence subSequence (int start, int end) {
+  //   ;
+  // }
+  // public String toString () {
+  //   ;
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
