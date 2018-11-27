80a8591cc6f2d638c36a7659d6e681a5d10f2cd7
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2018-11-26 21:59:40 -0500

    Cause the driver has to be in main

--- MyString.java
+++ MyString.java
@@ -1,6 +1,87 @@
 public class MyString implements CharSequence, Comparable<CharSequence>{
 private char[] data;
+public static void main(String[] args) {
 
+    // sole constructor test
+    MyString msg = new MyString("droid");
+    System.out.println("MyString msg = new MyString(\"charlotte\")");
+
+    // toString()
+    System.out.println("msg: " + msg);                                                  // "droid"
+
+    // length()
+    System.out.println("msg.length(): " + msg.length());                                // 5
+
+    System.out.println();
+
+    // charAt() exception catch
+    System.out.println("msg.charAt(-1):");
+    try {
+      System.out.println(msg.charAt(-1));                             // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    System.out.println("msg.charAt(15):");
+    try {
+      System.out.println(msg.charAt(15));                             // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // charAt()
+    System.out.println("msg.charAt(0): " + msg.charAt(0));                              // "d"
+    System.out.println("msg.charAt(3): " + msg.charAt(3));                              // "i"
+
+    System.out.println();
+
+    // subSequence() exception catch
+    // start < 0
+    System.out.println("msg.subSequence(-1,4):");
+    try {
+      System.out.println(msg.subSequence(-1,4));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // end < 0
+    System.out.println("msg.subSequence(1,-4):");
+    try {
+      System.out.println(msg.subSequence(1,-4));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // start > end
+    System.out.println("msg.subSequence(11,4):");
+    try {
+      System.out.println(msg.subSequence(11,4));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // end > length()
+    System.out.println("msg.subSequence(1,14):");
+    try {
+      System.out.println(msg.subSequence(1,14));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // subSequence()
+    System.out.println("msg.subSequence(0,3): " + msg.subSequence(0,3));                // "dro"
+    System.out.println("msg.subSequence(2,5): " + msg.subSequence(2,5));                // "oid"
+
+    System.out.println();
+
+    // compareTo()
+    System.out.println("msg.compareTo(\"droid\"): " + msg.compareTo("droid"));          // 0
+    System.out.println("msg.compareTo(\"bat\"): " + msg.compareTo("bat"));              // 1
+    System.out.println("msg.compareTo(\"seventh\"): " + msg.compareTo("seventh"));      // -1
+    System.out.println("msg.compareTo(\"drank\"): " + msg.compareTo("drank"));          // 1
+    System.out.println("msg.compareTo(\"drunk\"): " + msg.compareTo("drunk"));          // -1
+
+  }
 
 public MyString (CharSequence s){
   data  = new char [s.length()];
@@ -32,7 +113,7 @@ public char charAt (int index) {
   public CharSequence subSequence (int start, int end) {
 //Catches an error if the start or end is wrong
     if ((start < 0 || start > end) || (end < 0 || end > data.length)) {
-        throw new IndexOutOfBoundsException ("Sigh");
+        throw new IndexOutOfBoundsException ("Start and end inputs can't be negative. The end input can't be greater than the length of the CharSequence");
       }
     if (start == end){return "";}
     String sup = "";
@@ -50,30 +131,30 @@ public char charAt (int index) {
 
   public int compareTo (CharSequence e){
     if (this.length() == 0 && e.length() == 0) {return 0;}
-    if(this.length() == 0){return -7;}
-    if(e.length() == 0){return 100;}
+    if(this.length() == 0){return -1;}
+    if(e.length() == 0){return 1;}
     if(this.length() < e.length()){
       for (int i = 0; i < this.length();i ++){
         if(this.charAt(i) != e.charAt(i)){
           if (this.charAt(i) * 1 - e.charAt(i) * 1 > 0){
-            return 100;
+            return 1;
           }
-          else {return -7;}}}
+          else {return -1;}}}
     }
     if(this.length() > e.length()){
       for (int i = 0; i < e.length();i ++){
         if(this.charAt(i) != e.charAt(i)){
           if (this.charAt(i) * 1 - e.charAt(i) * 1 > 0){
-            return 100;
+            return 1;
           }
-          else {return -7;}}}
+          else {return -1;}}}
     }
     for (int i = 0; i < this.length();i ++){
       if(this.charAt(i) != e.charAt(i)){
         if (this.charAt(i) * 1 - e.charAt(i) * 1 > 0){
-          return 100;
+          return 1;
         }
-        else {return -7;}
+        else {return -1;}
       }}
       return 0;
     }
